08527e133d601bebbd76368b2ebf2fd054415b90
Quentin Arehart <104207952+arehart95@users.noreply.github.com>
2024-12-07 19:15:54 -0500

    Update GetChange.cpp
    Make optimizations with const and constexpr
    Exchange inner for loop for range-based for loop
    Add dialogue

--- Algorithmic Design and Technique/Greedy Algorithms/GetChange.cpp
+++ Algorithmic Design and Technique/Greedy Algorithms/GetChange.cpp
@@ -1,39 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
 using namespace std;
 
 // Figure out how many coins you need to make the inputted value. 
 
-int changeMoney(int V) {
-    int coins[] = {1, 5, 10, 25};
-    int m = sizeof(coins)/sizeof(coins[0]);
+int changeMoney(const int value) {
+    constexpr int coins[] = {1, 5, 10, 25};
+    constexpr int m = std::size(coins);
 
-    int wallet[V + 1];
+    int wallet[value + 1];
     wallet[0] = 0; // base case
 
-    for(int i = 1; i <= V; i++) {
+    for(int i = 1; i <= value; i++) {
         wallet[i] = INT_MAX;
     }
 
-    for (int i = 1; i <= V; i++)
-        for (int j = 0; j < m; j++)
-            if (coins[j] <= i) {
-                int sub_result = wallet[i - coins[j]];
-                if (sub_result != INT_MAX && sub_result + 1 < wallet[i])
+    for (int i = 1; i <= value; i++)
+        for (const int coin : coins)
+            if (coin <= i) {
+                if (const int sub_result = wallet[i - coin]; 
+                        sub_result != INT_MAX && sub_result + 1 < wallet[i])
                     wallet[i] = sub_result + 1;
             }
 
-    if (wallet[V] == INT_MAX)
+    if (wallet[value] == INT_MAX)
         return -1;
 
-    return wallet[V];
+    return wallet[value];
 }
 
   int main() {
 
     int V{0};
+    cout << "Please enter the amount of change: ";
     cin >> V;
-
-    cout << changeMoney(V);
+    cout << "You need " << changeMoney(V) << " coins to make this amount.";
 
     return 0;
 }
